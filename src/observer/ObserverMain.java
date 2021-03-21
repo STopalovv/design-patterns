ab0aab03bada7d938bf0b403453f02a47574da4d
stopalovv <stopalovv@gmail.com>
2021-03-21 20:43:44 +0200

    Initial commit

--- src/observer/ObserverMain.java
+++ src/observer/ObserverMain.java
@@ -4,14 +4,14 @@ public class ObserverMain {
 
     public static void main(String[] args) {
         Instructor instructor = new Instructor();
-        Observer observer1 = new Viewer("Viewer 1");
-        Observer observer2 = new Viewer("Viewer 2");
-        Observer observer3 = new Viewer("Viewer 2");
+        Observer observer1 = new Viewer("George");
+        Observer observer2 = new Viewer("Peter");
+        Observer observer3 = new Viewer("Mark");
 
         instructor.subscribe(observer1);
         instructor.subscribe(observer2);
         instructor.subscribe(observer3);
 
-        instructor.setExercise("New exercise has been released");
+        instructor.setExercise("Bench press");
     }
 }
